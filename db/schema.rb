41f19fa72d2086efde44e9ad6879cc4ed422b736
geordie-henderson-hs <geordie.henderson@hootsuite.com>
2017-06-18 21:28:18 -0700

    Updated schema.rb to format for current version of rails

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,9 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170408230900) do
 
-  create_table "projects", force: true do |t|
+  create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.text     "description"
+    t.text     "description",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "start_date"
@@ -24,43 +23,40 @@ ActiveRecord::Schema.define(version: 20170408230900) do
     t.string   "more_info"
     t.string   "project_handle"
     t.integer  "team_id"
+    t.index ["team_id"], name: "index_projects_on_team_id", using: :btree
   end
 
-  add_index "projects", ["team_id"], name: "index_projects_on_team_id", using: :btree
-
-  create_table "reports", force: true do |t|
+  create_table "reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.text     "summary"
+    t.text     "summary",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "team_updates", force: true do |t|
+  create_table "team_updates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.text     "summary"
+    t.text     "summary",    limit: 65535
     t.integer  "week"
     t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "team_id"
     t.string   "email"
+    t.index ["team_id"], name: "index_team_updates_on_team_id", using: :btree
   end
 
-  add_index "team_updates", ["team_id"], name: "index_team_updates_on_team_id", using: :btree
-
-  create_table "teams", force: true do |t|
+  create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
+    t.text     "description",        limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "parent_team_id"
-    t.integer  "team_updates_count", default: 0
+    t.integer  "team_updates_count",               default: 0
+    t.index ["name"], name: "index_teams_on_name", unique: true, using: :btree
+    t.index ["parent_team_id"], name: "index_teams_on_parent_team_id", using: :btree
   end
 
-  add_index "teams", ["name"], name: "index_teams_on_name", unique: true, using: :btree
-  add_index "teams", ["parent_team_id"], name: "index_teams_on_parent_team_id", using: :btree
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
@@ -71,10 +67,10 @@ ActiveRecord::Schema.define(version: 20170408230900) do
     t.string   "email"
   end
 
-  create_table "work_items", force: true do |t|
+  create_table "work_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "item_key"
     t.string   "project"
-    t.text     "summary"
+    t.text     "summary",         limit: 65535
     t.string   "status"
     t.integer  "statusCategory"
     t.integer  "week"
@@ -83,19 +79,18 @@ ActiveRecord::Schema.define(version: 20170408230900) do
     t.string   "assignee"
     t.integer  "num_people"
     t.string   "people"
-    t.integer  "workItemType",    default: 0
+    t.integer  "workItemType",                  default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "fix_version"
     t.datetime "updated"
     t.datetime "created"
     t.string   "objective"
-    t.boolean  "customer_facing", default: true
+    t.boolean  "customer_facing",               default: true
     t.integer  "team_id"
     t.string   "product"
+    t.index ["item_key"], name: "index_work_items_on_item_key", using: :btree
+    t.index ["team_id"], name: "index_work_items_on_team_id", using: :btree
   end
-
-  add_index "work_items", ["item_key"], name: "index_work_items_on_item_key", using: :btree
-  add_index "work_items", ["team_id"], name: "index_work_items_on_team_id", using: :btree
 
 end
